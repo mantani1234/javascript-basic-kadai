0364b11251addd9990bdb688d530b4b97ed01442
mantani1234 <mantani-daiki@i.softbank.jp>
2026-06-30 19:30:32 +0900

    Fix date retrieval and correct method names

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -3,8 +3,8 @@ const today= new Date();
 //年を取得
 const year= today.getFullYear();
 //月を取得
-const month = today.getmonth()+1;//月は０から始まるため＋１
+const month = today.getMonth()+1;//月は０から始まるため＋１
 //日を取得
-const date = birthday.getDate();
+const date = today.getDate();
 
-console.log(year+"年"+month+月"+date+"日");
+console.log(year+"年"+month+"月"+date+"日");
