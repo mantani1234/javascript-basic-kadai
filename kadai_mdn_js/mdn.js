5bd8afb36ba285b10a92493f18084d7ad61a44ca
mantani1234 <mantani-daiki@i.softbank.jp>
2026-06-30 19:29:13 +0900

    Update date handling to use today's date

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,4 +1,10 @@
-const dateControl = document.querySelector('input[type="date"]');
-dateControl.value = "2026-06-30";
-console.log(dateControl.value); 
-console.log(dateControl.valueAsNumber); 
+//今日の日付を取得
+const today= new Date();
+//年を取得
+const year= today.getFullYear();
+//月を取得
+const month = today.getmonth()+1;//月は０から始まるため＋１
+//日を取得
+const date = birthday.getDate();
+
+console.log(year+"年"+month+月"+date+"日");
